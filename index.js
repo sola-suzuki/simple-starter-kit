3d2f4d159daff1271d479c0a36e23550ca6b3f81
sola-suzuki <102899921+sola-suzuki@users.noreply.github.com>
2022-04-07 04:04:33 +0900

    fix confirm directory content

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const dest = `${path.resolve()}`;
 
 const copy = function(src, dest) {
   if (fs.existsSync(src)) {
-    const stats = fs.existsSync(src) && fs.statSync(src);
-    const isDirectory = fs.existsSync(src) && stats.isDirectory();
+    const stats = fs.statSync(src);
+    const isDirectory = stats.isDirectory();
 
-    if (fs.existsSync(src) && isDirectory) {
+    if (isDirectory) {
       !fs.existsSync(dest) && fs.mkdirSync(dest);
       fs.readdirSync(src).forEach(function(key) {
         copy(path.join(src, key), path.join(dest, key));
@@ -20,6 +20,10 @@ const copy = function(src, dest) {
       fs.linkSync(src, dest);
     }
   }
+}
+
+if (fs.readdirSync(dest).length) {
+  throw new Error('Directory not empty')
 }
 
 copy(src, dest)
