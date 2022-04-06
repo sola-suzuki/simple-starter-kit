24ec3e063ffd78e3ed06288ede3f1672b4020588
sola-suzuki <102899921+sola-suzuki@users.noreply.github.com>
2022-04-07 02:39:53 +0900

    fix dirsync

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 #! /usr/bin/env node
 
-const copy = require('fs-copy');
+const fs = require('fs');
+const path = require('path');
 
-copy(`${__dirname}/src`, process.cwd());
+const src = `${__dirname}/src`;
+const dest = `${path.resolve()}`;
+
+const copy = function(src, dest) {
+  if (fs.existsSync(src)) {
+    const stats = fs.existsSync(src) && fs.statSync(src);
+    const isDirectory = fs.existsSync(src) && stats.isDirectory();
+
+    if (fs.existsSync(src) && isDirectory) {
+      !fs.existsSync(dest) && fs.mkdirSync(dest);
+      fs.readdirSync(src).forEach(function(key) {
+        copy(path.join(src, key), path.join(dest, key));
+      });
+    } else {
+      fs.linkSync(src, dest);
+    }
+  }
+}
+
+copy(src, dest)
